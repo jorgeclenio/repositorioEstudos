0ec42fe4a6fbb621a4ed001186e4fd25aeeb8218
Jorge Clênio <cleniocontato@gmail.com>
2021-08-26 02:34:06 -0300

    new update

---
+++ 001.Digital Innovation One/005.Introdução à biblioteca jQuery/js/main.js
@@ -0,0 +1,22 @@
+
+function consultaCep() {
+
+    var cep = document.getElementById("cep").value;
+    var url = ("http://viacep.com.br/ws/" + cep + "/json/");
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (response) {
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#ddd").html(response.ddd);
+            $("#tituloCep").html("CEP " + response.cep);
+        }
+    })
+}
+
+
+
