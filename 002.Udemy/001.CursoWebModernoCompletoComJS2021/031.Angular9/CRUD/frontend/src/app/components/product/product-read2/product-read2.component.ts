484a0bfd234a1ad19e53df4888a108d1467e63de
Jorge Clênio <cleniocontato@gmail.com>
2022-04-02 12:17:54 -0300

    02/04/2022

--- 002.Udemy/001.CursoWebModernoCompletoComJS2021/031.Angular9/CRUD/frontend/src/app/components/product/product-read2/product-read2.component.ts
+++ 002.Udemy/001.CursoWebModernoCompletoComJS2021/031.Angular9/CRUD/frontend/src/app/components/product/product-read2/product-read2.component.ts
@@ -1,13 +1,16 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { ProductRead2DataSource, ProductRead2Item } from './product-read2-datasource';
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTable } from "@angular/material/table";
+import {
+  ProductRead2DataSource,
+  ProductRead2Item,
+} from "./product-read2-datasource";
 
 @Component({
-  selector: 'app-product-read2',
-  templateUrl: './product-read2.component.html',
-  styleUrls: ['./product-read2.component.css']
+  selector: "app-product-read2",
+  templateUrl: "./product-read2.component.html",
+  styleUrls: ["./product-read2.component.css"],
 })
 export class ProductRead2Component implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -16,7 +19,7 @@ export class ProductRead2Component implements AfterViewInit {
   dataSource: ProductRead2DataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ["id", "name"];
 
   constructor() {
     this.dataSource = new ProductRead2DataSource();
